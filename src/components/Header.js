711ce8c709c91314237d5dc83cd352ac7c569b58
Nguyen Minh Nhat Pham <phamngu@deakin.edu.au>
2019-11-13 13:16:26 +1100

    Make the logo smaller

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 const Header = () => (
     <Link to='/'>
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" width="65px" height="65px" />
     </Link>
 )
 
